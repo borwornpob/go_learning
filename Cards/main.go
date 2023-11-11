81dc19467d09ac4520169663853459c5e46ac18f
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-11-11 14:21:38 +0700

    Initial Commit

---
+++ Cards/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//var someValue int = 64
+
+func main() {
+	//var card string = "Ace of Spades"
+	//card := "Ace of Spades"
+	//card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
