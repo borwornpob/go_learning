f9b8f7b4ba4bf0254c21d2456cc23ff84e3b7e21
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-11-12 18:46:20 +0700

    Finish Cards project

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"testing"
+	"os"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Error("Expected deck length of 48,  but got", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("Expected first card of Ace of Spades, but got", d[0])
+	}
+
+	if d[len(d) - 1] != "Kings of Clubs" {
+		t.Error("Expected last card of Kings of Clubs, but got", d[len(d) - 1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Error("Expected 52 cards in deck, got ", len(loadedDeck))
+	}
+	
+	os.Remove("_decktesting")
+
+}
--- Cards/main.go
+++ Cards/main.go
@@ -1,23 +1,7 @@
 package main
 
-//var someValue int = 64
-
 func main() {
-	//var card string = "Ace of Spades" 
-	//card := "Ace of Spades"
-	//card := newCard()
-	// cards := deck{"Ace of Diamonds", newCard()}
-	// cards = append(cards, "Six of Spades")
-
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
-
-	cards := newDeck()
-	
-	_, remainingDeck := deal(cards, 4)
-	remainingDeck.saveToFile("my_cards")
-	//hand.print()
-	//fmt.Println(remainingDeck.toString())
-	
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
